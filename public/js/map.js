c61906809d78dfe2e19ed2f8e9dab4209254a959
Polina <bad.black.neko.xx@gmail.com>
2022-10-12 12:35:11 +0300

    Tags on map added

--- public/js/map.js
+++ public/js/map.js
@@ -1,15 +1,33 @@
 ymaps.ready(function () {
   var myMap = new ymaps.Map("YMapsID", {
-    center: [55.76, 37.64],
-    zoom: 10
+    center: [0, 0],
+    zoom: 1
   });
-//   // Создает метку в центре Москвы
-// var placemark = new YMaps.Placemark(new YMaps.GeoPoint(37.609218,55.753559), {hideIcon: false});
 
-// // Устанавливает содержимое балуна
-// placemark.name = "Москва";
-// placemark.description = "Столица Российской Федерации";
 
-// // Добавляет метку на карту
-// map.addOverlay(placemark); 
+  var myPlacemarkRussia = new ymaps.Placemark([55.55, 37.37], {}, {
+    iconLayout: 'default#image',
+    iconImageHref: './img/lapa.png',
+    iconImageSize: [20, 20],
+    iconImageOffset: [-3, -42],
+    // iconCaption: 'Russia'
+  });
+
+  myPlacemarkRussia.properties.set({
+    // Подпись к диаграмме.
+    iconCaption: 'Russia',
+ });
+
+  myMap.geoObjects.add(myPlacemarkRussia);
+
+  var myPlacemarkTurkey = new ymaps.Placemark([39.55, 32.51], {}, {
+    iconLayout: 'default#image',
+    iconImageHref: './img/lapa.png',
+    iconImageSize: [20, 20],
+    iconImageOffset: [-3, -42],
+    iconCaption: 'Turkey',
+  });
+
+  myMap.geoObjects.add(myPlacemarkTurkey);
+
 });
